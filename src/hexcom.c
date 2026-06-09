7cb87d65b08e24bbbea38c2a3c6b3afac7aa0ca8
Jeffrey H. Johnson <johnsonjh.dev@gmail.com>
2026-06-09 19:48:16 -0400

    Appease linters
    Signed-off-by: Jeffrey H. Johnson <johnsonjh.dev@gmail.com>

--- src/hexcom.c
+++ src/hexcom.c
@@ -332,12 +332,11 @@ main (int argc, char **argv)
     {
       size_t pad_start = (size_t)first_addr + span;
       size_t pad_end = (size_t)first_addr + (size_t)records * RECSZ;
-      const size_t max_addr = (size_t)ADDRSP;
 
-      if (pad_start < max_addr)
+      if (pad_start < (size_t)ADDRSP)
         {
-          if (pad_end > max_addr)
-            pad_end = max_addr;
+          if (pad_end > (size_t)ADDRSP)
+            pad_end = (size_t)ADDRSP;
 
           (void)memset (image + pad_start, 0x1A, pad_end - pad_start);
         }
