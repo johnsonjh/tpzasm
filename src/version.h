73321cd4601e4ce76b6b7e94a0f862c1d0ec0994
Jeffrey H. Johnson <johnsonjh.dev@gmail.com>
2026-06-12 11:04:16 -0400

    Release 0.34
    Signed-off-by: Jeffrey H. Johnson <johnsonjh.dev@gmail.com>

--- src/version.h
+++ src/version.h
@@ -18,7 +18,7 @@
 
 /******************************************************************************/
 
-# define ASM_VERSION "Release 0.33"
+# define ASM_VERSION "Release 0.34"
 # define ASM_URL " https://github.com/johnsonjh/tpzasm\n"
 
 /******************************************************************************/
