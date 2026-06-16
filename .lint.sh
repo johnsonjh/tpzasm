124f3c653774b8a0aba777367b98e55db0769724
Jeffrey H. Johnson <johnsonjh.dev@gmail.com>
2026-06-16 18:46:13 -0400

    Further appease linters
    Signed-off-by: Jeffrey H. Johnson <johnsonjh.dev@gmail.com>

--- .lint.sh
+++ .lint.sh
@@ -889,7 +889,7 @@ esac
 if [ "${rc}" = 0 ]; then
   "${MAKE:-make}" distclean > /dev/null 2>&1 || :
   printf '\n%s\n\n' ">>>>>>>>>>>>>>>> lint SUCCESSFUL <<<<<<<<<<<<<<<<"
-  # shellcheck disable=SC2065
+  # shellcheck disable=SC2015,SC2065
   test -x ./.timestamp.sh > /dev/null 2>&1 \
     && ./.timestamp.sh > /dev/null 2>&1 || :
 else
