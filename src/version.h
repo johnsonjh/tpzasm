21ea1cd47b59830bc627b2e14ebc430eaaa3494c
Jeffrey H. Johnson <johnsonjh.dev@gmail.com>
2026-06-16 14:07:58 -0400

    Bump to 0.78
    Signed-off-by: Jeffrey H. Johnson <johnsonjh.dev@gmail.com>

--- src/version.h
+++ src/version.h
@@ -18,7 +18,7 @@
 
 /******************************************************************************/
 
-# define ASM_VERSION "Release 0.77"
+# define ASM_VERSION "Release 0.78"
 # define ASM_URL " https://github.com/johnsonjh/tpzasm\n"
 
 /******************************************************************************/
