a0759789e47eaf1958ff818c4c0f1f433d9cf08f
Jeffrey H. Johnson <johnsonjh.dev@gmail.com>
2026-06-17 01:45:03 -0400

    Bump version and update README.md
    Signed-off-by: Jeffrey H. Johnson <johnsonjh.dev@gmail.com>

--- src/version.h
+++ src/version.h
@@ -18,7 +18,7 @@
 
 /******************************************************************************/
 
-# define ASM_VERSION "Release 0.79"
+# define ASM_VERSION "Release 0.80-dev"
 # define ASM_URL " https://github.com/johnsonjh/tpzasm\n"
 
 /******************************************************************************/
